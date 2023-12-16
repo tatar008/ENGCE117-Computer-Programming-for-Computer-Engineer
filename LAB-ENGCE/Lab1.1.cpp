8a067b6c49dccd6fe4956bf671a40ce113551042
tatar <136808495+tatar008@users.noreply.github.com>
2023-12-16 11:46:36 +0700

    Add files via upload

---
+++ LAB-ENGCE/Lab1.1.cpp
@@ -0,0 +1,12 @@
+int main() {
+	int n ;
+    int i ;
+    printf( "Input : " ) ;
+    scanf( "%d", &n ) ;
+    
+    for( i = 1 ; i <= n ; i++ ) {
+    	printf( "[%d] Hello world", i ) ;
+    }
+    return 0;
+}
+}
